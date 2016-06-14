13db954ad112fa7638dace3606aa083443dc20c0
Rafael Salgueiroza <tiorafa2005@gmail.com>
2016-06-13 21:38:29 -0300

    Adicionando arquivo que faltou

---
+++ Uniplac.Mercado.Infra.Dados/Repositorios/ProdutoRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniplac.Mercado.Dominio;
+using Uniplac.Mercado.Dominio.Contratos;
+
+namespace Uniplac.Mercado.Infra.Dados.Repositorios
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        public Produto Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto Buscar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
