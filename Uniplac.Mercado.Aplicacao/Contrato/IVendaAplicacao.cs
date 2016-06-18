a870fd664169df353d8d456b242c984a5376e04b
luanaandra <luanar12@hotmail.com>
2016-06-18 18:17:36 -0300

    Adicionando Classe VendaAplicação

---
+++ Uniplac.Mercado.Aplicacao/Contrato/IVendaAplicacao.cs
@@ -0,0 +1,13 @@
+﻿using Uniplac.Mercado.Dominio;
+
+namespace Uniplac.Mercado.Aplicacao.Contrato
+{
+    public interface IVendaAplicacao
+    {
+
+        Venda CriarVenda(Venda venda);
+        Venda Atualizar(Venda venda);
+        Venda Deletar(Venda venda);
+        Venda Busca(int id);
+    }
+}
---
+++ Uniplac.Mercado.Aplicacao/Serviço/VendaAplicacao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Uniplac.Mercado.Aplicacao.Contrato;
+using Uniplac.Mercado.Dominio;
+using Uniplac.Mercado.Dominio.Contratos;
+
+namespace Uniplac.Mercado.Aplicacao.Serviço
+{
+    public class VendaAplicacao : IVendaAplicacao
+    {
+       
+            private IVendaRepository _repositorio;
+
+            public VendaAplicacao(IVendaRepository repositorio)
+            {
+                this._repositorio = repositorio;
+            }
+
+            public Venda Atualizar(Venda venda)
+            {
+                Venda vendaAtualizada = _repositorio.Atualizar(venda);
+
+                return vendaAtualizada;
+            }
+
+            public Venda Busca(int id)
+            {
+                Venda venda = _repositorio.Buscar(id);
+                return venda;
+            }
+
+            public Venda CriarVenda(Venda venda)
+            {
+                return _repositorio.Adicionar(venda);
+            }
+
+            public Venda Deletar(Venda venda)
+            {
+                _repositorio.Deletar(venda);
+                return venda;
+            }
+        }
+    }
+
