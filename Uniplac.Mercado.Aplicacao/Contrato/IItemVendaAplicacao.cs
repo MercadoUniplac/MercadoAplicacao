1340f844837b732de307ac16c88085aea7bab5ed
luanaandra <luanar12@hotmail.com>
2016-06-18 14:20:28 -0300

    Adicionando arquivos faltantes.

---
+++ Uniplac.Mercado.Aplicacao/Contrato/IItemVendaAplicacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniplac.Mercado.Dominio;
+
+namespace Uniplac.Mercado.Aplicacao.Contrato
+{
+    public interface IItemVendaAplicacao
+    {
+
+        ItemVenda CriarItemVenda(ItemVenda itemVenda);
+        ItemVenda Atualizar(ItemVenda itemVenda);
+        ItemVenda Deletar(ItemVenda itemVenda);
+        ItemVenda Busca(int id);
+    }
+}
---
+++ Uniplac.Mercado.Aplicacao/Serviço/ItemVendaAplicacao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniplac.Mercado.Aplicacao.Contrato;
+using Uniplac.Mercado.Dominio;
+using Uniplac.Mercado.Dominio.Contratos;
+
+namespace Uniplac.Mercado.Aplicacao.Serviço
+{
+    public class ItemVendaAplicacao : IItemVendaAplicacao
+    {
+        private IItemVendaRepository _repositorio;
+
+        public ItemVendaAplicacao(IItemVendaRepository repositorio)
+        {
+            this._repositorio = repositorio;
+        }
+
+        public ItemVenda Atualizar(ItemVenda itemVenda)
+        {
+            ItemVenda itemVendaAtualizado = _repositorio.Atualizar(itemVenda);
+
+            return itemVendaAtualizado;
+        }
+
+        public ItemVenda Busca(int id)
+        {
+            ItemVenda itemVenda = _repositorio.Buscar(id);
+            return itemVenda;
+        }
+
+        public ItemVenda CriarItemVenda(ItemVenda itemVenda)
+        {
+            return _repositorio.Adicionar(itemVenda);
+        }
+
+        public ItemVenda Deletar(ItemVenda itemVenda)
+        {
+            _repositorio.Deletar(itemVenda);
+            return itemVenda;
+        }
+
+    }
+}
